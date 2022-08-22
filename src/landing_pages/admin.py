52800cd69dad0729bff517e03191140dcae4aecc
CFE <hello@teamcfe.com>
2022-08-22 11:24:57 -0500

    22 - Making Changes to Models

--- src/landing_pages/admin.py
+++ src/landing_pages/admin.py
@@ -3,7 +3,9 @@ from django.contrib import admin
 from .models import LandingPageEntry
 
 class LandingPageEntryAdmin(admin.ModelAdmin):
-    list_display = ['name', 'email']
+    list_display = ['email', 'name', 'timestamp']
     search_fields = ['name', 'email']
+    readonly_fields = ['name', 'email', 'timestamp'] # 'updated']
+    list_filter = ['timestamp']
 
 admin.site.register(LandingPageEntry, LandingPageEntryAdmin)
--- src/landing_pages/models.py
+++ src/landing_pages/models.py
@@ -2,8 +2,11 @@ from django.db import models
 
 
 class LandingPageEntry(models.Model):
-    name = models.CharField(max_length=220)
+    name = models.CharField(max_length=220, blank=True)
     email = models.EmailField()
+    active = models.BooleanField(default=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    # updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.email}"
