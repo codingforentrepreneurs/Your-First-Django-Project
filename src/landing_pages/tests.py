6bbcc7c384c98c53140618993d8eba41d70ba98f
CFE <hello@teamcfe.com>
2022-08-22 11:35:08 -0500

    23 - Your First Automated Django Test

--- src/landing_pages/tests.py
+++ src/landing_pages/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
 
 # Create your tests here.
+from .models import LandingPageEntry
+
+class LandingPageEntryTestCase(TestCase):
+    def setUp(self):
+        self.inactive_count = 3
+        for i in range(0, self.inactive_count):
+            LandingPageEntry.objects.create(
+                name='Justin',
+                email='abc@gmail.com',
+                active=False
+            )
+        
+    
+    def test_inactive(self):
+        # obj_list = LandingPageEntry.objects.all()
+        # inactive_items = [x for x in obj_list if not x.active]
+        # assert len(inactive_items) == 2
+        qs = LandingPageEntry.objects.filter(active=False)
+        # assert qs.count() == 1
+        self.assertEqual(qs.count(), self.inactive_count)
