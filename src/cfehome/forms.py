3a58e13cd9dfe06afff61c1df725508f74f203e3
CFE <hello@teamcfe.com>
2022-08-22 09:29:19 -0500

    15 - Styling Django Forms

--- src/cfehome/forms.py
+++ src/cfehome/forms.py
@@ -6,6 +6,20 @@ class LandingPageForm(forms.Form):
     email = forms.EmailField()
     email2 = forms.EmailField(label='Confirm Email')
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields:
+            default_css_class = 'form-control' # bootstrap
+            new_attrs = {
+                "class": default_css_class,
+                "id": f"{field}",
+                "placeholder": f"Your {field}",
+            }
+            if field == "email2":
+                new_attrs['placeholder'] = f"Confirm your email"
+            self.fields[field].widget.attrs.update(new_attrs)
+
+
     def clean(self):
         data = self.cleaned_data
         email = data.get("email")
