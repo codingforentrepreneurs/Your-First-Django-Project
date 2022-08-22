0468ce60e948a25e723456c0b74295b050059d21
CFE <hello@teamcfe.com>
2022-08-22 11:55:07 -0500

    24 - Testing with Backup Data

--- src/landing_pages/tests.py
+++ src/landing_pages/tests.py
@@ -1,9 +1,14 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
 # Create your tests here.
 from .models import LandingPageEntry
 
+User = get_user_model()
+
 class LandingPageEntryTestCase(TestCase):
+    fixtures = ["entry-data.json", "users.json"]
+
     def setUp(self):
         self.inactive_count = 3
         for i in range(0, self.inactive_count):
@@ -20,4 +25,14 @@ class LandingPageEntryTestCase(TestCase):
         # assert len(inactive_items) == 2
         qs = LandingPageEntry.objects.filter(active=False)
         # assert qs.count() == 1
+        self.assertTrue(qs.exists())
         self.assertEqual(qs.count(), self.inactive_count)
+
+    def test_active(self):
+        qs = LandingPageEntry.objects.filter(active=True)
+        self.assertTrue(qs.exists())
+        self.assertGreaterEqual(qs.count(), 5)
+
+    def test_users_exist(self):
+        qs = User.objects.all()
+        self.assertTrue(qs.exists())
