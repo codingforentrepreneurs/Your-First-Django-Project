1cd78249fb21d744f6289506a5008da1b4cb6757
CFE <hello@teamcfe.com>
2022-08-22 11:01:09 -0500

    20 - Saving to a Model in a View

--- src/landing_pages/views.py
+++ src/landing_pages/views.py
@@ -1,6 +1,7 @@
 # from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import LandingPageEntry
 from .forms import LandingPageForm
 
 def home_page(request, *args, **kwargs):
@@ -11,6 +12,17 @@ def home_page(request, *args, **kwargs):
     form = LandingPageForm(request.POST or None)
     if form.is_valid():
         print(form.cleaned_data)
+        name = form.cleaned_data.get("name")
+        email = form.cleaned_data.get("email")
+        obj = LandingPageEntry.objects.create(name=name, email=email)
+        # obj.email = email
+        # obj.save()
+        
+        # obj2 = LandingPageEntry()
+        # obj2.name = name
+        # obj2.email = email
+        # obj2.save()
+
         form = LandingPageForm()
     # print("your email is", request.POST.get("email"))
 
